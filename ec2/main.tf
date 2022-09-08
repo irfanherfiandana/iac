000f4b3caf9610e3cc2b57a2d6cd91f35cec1e49
Irfan Herfiandana <irfanherfiandana@gmail.com>
2022-09-08 13:42:06 +0000

    add elasticsearch ec2 and sg

---
+++ ec2/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ap-southeast-3"
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "elasticsearch"
+
+  ami                    = "ami-043f1fdec42408287"
+  instance_type          = "t3.micro"
+  key_name               = "irfan@macpro"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-05181b0765c3b2e0f"]
+  subnet_id              = "subnet-02f78ef1484b5cd42"
+  user_data              = "userdata.conf"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "test"
+  }
+}
+
---
+++ sg/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ap-southeast-3"
+}
+
+
+resource "aws_security_group" "test-sg" {
+  name = "test-sg"
+  description = "Allow SSH & Elasticsearch traffic via Terraform"
+  vpc_id = "vpc-03cb622497873e5b5"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9200
+    to_port     = 9200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
